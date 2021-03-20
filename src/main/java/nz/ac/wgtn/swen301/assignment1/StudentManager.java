f93a41b80d13c5972d9ad0cc2f70aab667c426e8
Yun Zhou <zhouyun@ecs.vuw.ac.nz>
2021-03-20 21:04:17 +0800

    update Student.readDegree() and write some javadoc.

--- src/main/java/nz/ac/wgtn/swen301/assignment1/StudentManager.java
+++ src/main/java/nz/ac/wgtn/swen301/assignment1/StudentManager.java
@@ -5,7 +5,9 @@ import java.sql.*;
 import java.util.Collection;
 
 /**
- * A student managers providing basic CRUD operations for instances of Student, and a read operation for instances of Degree.
+ * A student managers providing basic CRUD operations for instances of Student, and a read
+ * operation for instances of Degree.
+ *
  * @author jens dietrich
  */
 public class StudentManager {
@@ -20,70 +22,131 @@ public class StudentManager {
     // THE FOLLOWING METHODS MUST IMPLEMENTED :
 
     /**
-     * Return a student instance with values from the row with the respective id in the database.
-     * If an instance with this id already exists, return the existing instance and do not create a second one.
+     * Return a student instance with values from the row with the respective id in the
+     * database. If an instance with this id already exists, return the existing instance and
+     * do not create a second one.
+     *
      * @param id
-     * @return
-     * @throws NoSuchRecordException if no record with such an id exists in the database
-     * This functionality is to be tested in test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_readStudent (followed by optional numbers if multiple tests are used)
+     *            the string id to locate the Student object
+     * @return the student object with the specified id
+     *
+     * @throws NoSuchRecordException
+     *             if no record with such an id exists in the database This functionality is to
+     *             be tested in
+     *             test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_readStudent
+     *             (followed by optional numbers if multiple tests are used)
      */
     public static Student readStudent(String id) throws NoSuchRecordException {
         return null;
     }
 
     /**
-     * Return a degree instance with values from the row with the respective id in the database.
-     * If an instance with this id already exists, return the existing instance and do not create a second one.
+     * Return a degree instance with values from the row with the respective id in the
+     * database. If an instance with this id already exists, return the existing instance and
+     * do not create a second one.
+     *
      * @param id
-     * @return
-     * @throws NoSuchRecordException if no record with such an id exists in the database
-     * This functionality is to be tested in test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_readDegree (followed by optional numbers if multiple tests are used)
+     *            the String id that can locate the Degree
+     * @return the Degree object with the specified id
+     *
+     * @throws NoSuchRecordException
+     *             if no record with such an id exists in the database This functionality is to
+     *             be tested in
+     *             test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_readDegree
+     *             (followed by optional numbers if multiple tests are used)
      */
     public static Degree readDegree(String id) throws NoSuchRecordException {
+        try {
+            // estblish the connection to the directory
+            String jdbc_url = "jdbc:derby:memory:studentdb";
+            Connection connection = DriverManager.getConnection(jdbc_url);
+            // Create a Statement object to execute the query with.
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement
+                    .executeQuery("SELECT * FROM DEGREES WHERE ID = '" + id + "'");
+
+            if (resultSet == null) {
+                throw new NoSuchRecordException(
+                        "We didn't find the degree id: " + id + " in our database. ");
+            }
+            // iterate the resultSet until no more rows can be read
+            while (resultSet.next()) {
+                // get the corresponding info from the DEGREES database
+                String degreeID = resultSet.getString("id");
+                String degreeName = resultSet.getString("name");
+
+                // construct the Degree object by passing variables to the constructor
+                Degree degree = new Degree(degreeID, degreeName);
+                connection.close();// close connection to save the resourse
+                return degree;
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        // it should never return null
+        // it should either return the Degree object with the specificed id
+        // or throw a NoSuchRecordException
         return null;
     }
 
     /**
-     * Delete a student instance from the database.
-     * I.e., after this, trying to read a student with this id will result in a NoSuchRecordException.
+     * Delete a student instance from the database. I.e., after this, trying to read a student
+     * with this id will result in a NoSuchRecordException.
+     *
      * @param student
-     * @throws NoSuchRecordException if no record corresponding to this student instance exists in the database
-     * This functionality is to be tested in test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_delete
+     * @throws NoSuchRecordException
+     *             if no record corresponding to this student instance exists in the database
+     *             This functionality is to be tested in
+     *             test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_delete
      */
-    public static void delete(Student student) throws NoSuchRecordException {}
+    public static void delete(Student student) throws NoSuchRecordException {
+    }
 
     /**
-     * Update (synchronize) a student instance with the database.
-     * The id will not be changed, but the values for first names or degree in the database might be changed by this operation.
-     * After executing this command, the attribute values of the object and the respective database value are consistent.
-     * Note that names and first names can only be max 1o characters long.
-     * There is no special handling required to enforce this, just ensure that tests only use values with < 10 characters.
+     * Update (synchronize) a student instance with the database. The id will not be changed,
+     * but the values for first names or degree in the database might be changed by this
+     * operation. After executing this command, the attribute values of the object and the
+     * respective database value are consistent. Note that names and first names can only be
+     * max 1o characters long. There is no special handling required to enforce this, just
+     * ensure that tests only use values with < 10 characters.
+     *
      * @param student
-     * @throws NoSuchRecordException if no record corresponding to this student instance exists in the database
-     * This functionality is to be tested in test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_update (followed by optional numbers if multiple tests are used)
+     * @throws NoSuchRecordException
+     *             if no record corresponding to this student instance exists in the database
+     *             This functionality is to be tested in
+     *             test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_update
+     *             (followed by optional numbers if multiple tests are used)
      */
-    public static void update(Student student) throws NoSuchRecordException {}
-
+    public static void update(Student student) throws NoSuchRecordException {
+    }
 
     /**
-     * Create a new student with the values provided, and save it to the database.
-     * The student must have a new id that is not been used by any other Student instance or STUDENTS record (row).
-     * Note that names and first names can only be max 1o characters long.
-     * There is no special handling required to enforce this, just ensure that tests only use values with < 10 characters.
+     * Create a new student with the values provided, and save it to the database. The student
+     * must have a new id that is not been used by any other Student instance or STUDENTS
+     * record (row). Note that names and first names can only be max 1o characters long. There
+     * is no special handling required to enforce this, just ensure that tests only use values
+     * with < 10 characters.
+     *
      * @param name
      * @param firstName
      * @param degree
-     * @return a freshly created student instance
-     * This functionality is to be tested in test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_createStudent (followed by optional numbers if multiple tests are used)
+     * @return a freshly created student instance This functionality is to be tested in
+     *         test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_createStudent
+     *         (followed by optional numbers if multiple tests are used)
      */
-    public static Student createStudent(String name,String firstName,Degree degree) {
+    public static Student createStudent(String name, String firstName, Degree degree) {
         return null;
     }
 
     /**
      * Get all student ids currently being used in the database.
-     * @return
-     * This functionality is to be tested in test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_getAllStudentIds (followed by optional numbers if multiple tests are used)
+     *
+     * @return This functionality is to be tested in
+     *         test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_getAllStudentIds
+     *         (followed by optional numbers if multiple tests are used)
      */
     public static Collection<String> getAllStudentIds() {
         return null;
@@ -91,12 +154,13 @@ public class StudentManager {
 
     /**
      * Get all degree ids currently being used in the database.
-     * @return
-     * This functionality is to be tested in test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_getAllDegreeIds (followed by optional numbers if multiple tests are used)
+     *
+     * @return This functionality is to be tested in
+     *         test.nz.ac.wgtn.swen301.assignment1.TestStudentManager::test_getAllDegreeIds
+     *         (followed by optional numbers if multiple tests are used)
      */
     public static Iterable<String> getAllDegreeIds() {
         return null;
     }
-
 
 }
--- src/test/java/test/nz/ac/wgtn/swen301/assignment1/TestStudentManager.java
+++ src/test/java/test/nz/ac/wgtn/swen301/assignment1/TestStudentManager.java
@@ -1,19 +1,21 @@
 package test.nz.ac.wgtn.swen301.assignment1;
 
-import nz.ac.wgtn.swen301.assignment1.StudentManager;
-import nz.ac.wgtn.swen301.studentdb.Student;
-import nz.ac.wgtn.swen301.studentdb.StudentDB;
-import org.junit.Before;
-import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import nz.ac.wgtn.swen301.assignment1.StudentManager;
+import nz.ac.wgtn.swen301.studentdb.Degree;
+import nz.ac.wgtn.swen301.studentdb.Student;
+import nz.ac.wgtn.swen301.studentdb.StudentDB;
 
 public class TestStudentManager {
 
     // DO NOT REMOVE THE FOLLOWING -- THIS WILL ENSURE THAT THE DATABASE IS AVAILABLE
     // AND IN ITS INITIAL STATE BEFORE EACH TEST RUNS
     @Before
-    public  void init () {
+    public void init() {
         StudentDB.init();
     }
     // DO NOT REMOVE BLOCK ENDS HERE
@@ -23,5 +25,86 @@ public class TestStudentManager {
         Student student = new StudentManager().readStudent("id42");
         // THIS WILL INITIALLY FAIL !!
         assertNotNull(student);
+    }
+
+    /**
+     * Description: <br/>
+     * Check whether the fields of Degree object is correct or not.
+     * 
+     * @author Yun Zhou
+     * @throws Exception
+     */
+    @Test
+    public void test_readDegree() throws Exception {
+
+        Degree degree0 = StudentManager.readDegree("deg0");
+        assertNotNull(degree0);
+        // System.out.println(degree_0.getId() + "\n" + degree_0.getName());
+        assert degree0.getId().equals("deg0") && degree0.getName().equals("BSc Computer Science");
+
+        Degree degree1 = StudentManager.readDegree("deg1");
+        assertNotNull(degree1);
+        assert degree1.getId().equals("deg1") && degree1.getName().equals("BSc Computer Graphics");
+
+        Degree degree2 = StudentManager.readDegree("deg2");
+        assertNotNull(degree2);
+        assert degree2.getId().equals("deg2") && degree2.getName().equals("BE Cybersecurity");
+
+        Degree degree3 = StudentManager.readDegree("deg3");
+        assertNotNull(degree3);
+        // System.out.println(degree3.getId() + "\n" + degree3.getName());
+        assert degree3.getId().equals("deg3")
+                && degree3.getName().equals("BE Software Engineering");
+
+        Degree degree4 = StudentManager.readDegree("deg4");
+        assertNotNull(degree4);
+        assert degree4.getId().equals("deg4") && degree4.getName().equals("BSc Mathematics");
+
+        Degree degree5 = StudentManager.readDegree("deg5");
+        assertNotNull(degree5);
+        assert degree5.getId().equals("deg5") && degree5.getName().equals("BSc Chemistry");
+
+        Degree degree6 = StudentManager.readDegree("deg6");
+        assertNotNull(degree6);
+        assert degree6.getId().equals("deg6") && degree6.getName().equals("BA Art");
+
+        Degree degree7 = StudentManager.readDegree("deg7");
+        assertNotNull(degree7);
+        assert degree7.getId().equals("deg7") && degree7.getName().equals("BA Philosophy");
+
+        Degree degree8 = StudentManager.readDegree("deg8");
+        assertNotNull(degree8);
+        assert degree8.getId().equals("deg8") && degree8.getName().equals("BCom Finance");
+
+        Degree degree9 = StudentManager.readDegree("deg9");
+        assertNotNull(degree9);
+        assert degree9.getId().equals("deg9") && degree9.getName().equals("BCom Marketing");
+
+    }
+
+    @Test
+    public void test_readStudent() throws Exception {
+
+    }
+
+    @Test
+    public void test_delete() throws Exception {
+    }
+
+    @Test
+    public void test_update() throws Exception {
+    }
+
+    @Test
+    public void test_createStudent() throws Exception {
+    }
+
+    @Test
+    public void test_getAllStudentIds() throws Exception {
+    }
+
+    @Test
+    public void test_getAllDegreeIds() throws Exception {
+
     }
 }
