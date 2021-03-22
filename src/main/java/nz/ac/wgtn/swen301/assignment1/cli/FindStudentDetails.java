4cacdb58b9ca9234108aef104b78fe2c2e6f7cca
Yun Zhou <zhouyun@ecs.vuw.ac.nz>
2021-03-22 16:54:21 +0800

    add the java cp command into the javadoc and refactoring the println statement as the old one is ugly when it show up in the terminal.

--- src/main/java/nz/ac/wgtn/swen301/assignment1/cli/FindStudentDetails.java
+++ src/main/java/nz/ac/wgtn/swen301/assignment1/cli/FindStudentDetails.java
@@ -1,7 +1,6 @@
 package nz.ac.wgtn.swen301.assignment1.cli;
 
 import nz.ac.wgtn.swen301.assignment1.StudentManager;
-import nz.ac.wgtn.swen301.studentdb.Degree;
 import nz.ac.wgtn.swen301.studentdb.NoSuchRecordException;
 import nz.ac.wgtn.swen301.studentdb.Student;
 
@@ -13,8 +12,12 @@ public class FindStudentDetails {
      * Executable: the user will provide a student id as single argument, and if the details
      * are found, the respective details are printed to the console. E.g. a user could invoke
      * this by running
-     * <p>
      * "java -cp <someclasspath> nz.ac.wgtn.swen301.assignment1.cli.FindStudentDetails id42"
+     *
+     * I use the putty to connect to the my school account and run the command below,
+     * it can run successfully.(maybe you can just replace zhouyun to your username.
+     *
+     * java -cp /am/st-james/home1/zhouyun/git/Assignment1/target/classes:/am/st-james/home1/zhouyun/git/Assignment1/lib/studentdb-1.1.1.jar:/am/st-james/home1/zhouyun/git/Assignment1/lib/guava-29.0-jre.jar:/am/st-james/home1/zhouyun/.m2/repository/org/apache/derby/derby/10.14.2.0/derby-10.14.2.0.jar nz.ac.wgtn.swen301.assignment1.cli.FindStudentDetails id42
      *
      * @param arg
      *            arguments that is entered by the user. e.g.the student id: id42
@@ -22,23 +25,23 @@ public class FindStudentDetails {
      *             if the student with the id has not been found
      */
     public static void main(String[] arg) throws NoSuchRecordException {
-        if (arg == null) {
+        if (arg == null ) {
+            System.out.println("The argument is null, please enter the argument and try again.");
+            throw new NullPointerException("Please enter the id argument.");
+        }
+        if (arg.length<1){
+            System.out.println("Please enter the argument and try again.");
             throw new NullPointerException("Please enter the id argument.");
         }
 
-        String student_id = arg[0];
+        String student_id = arg[0];//only read the first argument
         Student student = StudentManager.readStudent(student_id);
         if (student != null) {
-            System.out.println("ID\tFirst Name\tLast Name\tDegree ID\tDegree Name");
-            System.out.print(student.getId());
-            System.out.print("\t");
-            System.out.print(student.getFirstName());
-            System.out.print("\t");
-            System.out.print(student.getName());
-            System.out.print("\t");
-            System.out.print(student.getDegree().getId());
-            System.out.print("\t");
-            System.out.print(student.getDegree().getName());
+            System.out.println("student ID:\t"+ student.getId() );
+            System.out.println("First Name:\t " + student.getFirstName());
+            System.out.println("Last Name:\t" + student.getName() );
+            System.out.println("Degree ID:\t" + student.getDegree().getId());
+            System.out.println("Degree Name:\t" + student.getDegree().getName());
         }
 
     }
